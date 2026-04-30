efacc6e09649d13c1a1a4fff05fd63e46630b3ff
Anh Đức <anhduc1345iz@gmail.com>
2026-04-30 14:17:34 +0700

    Hoan thanh infor student voi du lieu tinh tach data

--- lib/features/infor/ctrls/ctrls_inforStudent.dart
+++ lib/features/infor/ctrls/ctrls_inforStudent.dart
@@ -1,13 +1,20 @@
 import '../models/models_inforStudent.dart';
+import '../services/services_inforStudent.dart';
 
-class CtrlsInforstudent {
-  //bien cuc bo 
+class StudentController {
+  Map<String, String> getProfile() {
+    final data = StudentStaticData.getStudent().data;
 
-  //phuong thuc
-  static void getInforStudent(){
-
+    return {
+      "name": data.tenDayDu,
+      "maSv": data.maSv,
+      "ngaySinh": data.ngaySinh,
+      "gioiTinh": data.gioiTinh,
+      "lop": data.lop,
+      "khoa": data.khoa,
+      "heDaoTao": data.bacHeDaoTao,
+      "nganh": data.nganh,
+      "nienKhoa": data.nienKhoa,
+    };
   }
-
-
 }
-
